f44f3871d65ca0b339c35580a1a22b5cb8993f53
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-12-12 17:55:06 +0530

    148 sort list

--- _148SortList.java
+++ _148SortList.java
@@ -1,3 +1,88 @@
-public class _148SortList {
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class _148SortList {
+
+    class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(){
+
+        }
+        ListNode(int val, ListNode next){
+            this.val=val;
+            this.next=next;
+        }
+    }
+    public static void MergeSort(int[] arr, int si, int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        MergeSort(arr, si, mid );
+        MergeSort(arr,mid+1, ei );
+        Merge(arr, si, mid, ei);
+    }
+
+    public static void Merge(int arr[], int si,int mid,int ei){
+        int temp[]=new int[ei-si+1];
+
+        int i=si;
+        int j=mid+1;
+        int k=0;
+
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k++]=arr[i++];
+            }else{
+                temp[k++]=arr[j++];
+            }
+        }
+
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=ei){
+            temp[k++]=arr[j++];
+        }
+
+        for(i=si, k=0;k<temp.length;i++,k++){
+            arr[i]=temp[k];
+        }
+    }
+    public ListNode sortList(ListNode head) {
+        
+
+        ListNode ptr=head;
+        int size=0;
+        while(ptr!=null){
+            ptr=ptr.next;
+            size++;
+        }
+
+        ptr=head;
+        int arr[]=new int[size];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=ptr.val;
+            ptr=ptr.next;
+        }
+
+        MergeSort(arr, 0, arr.length-1);
+
+        ptr=head;
+        for(int i=0;i<arr.length;i++){
+            ptr.val=arr[i];
+            ptr=ptr.next;
+        }
+
+        return head;
+    }
 }
---
+++ _148SortList_Orderof1.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class _148SortList_Orderof1 {
+    
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+    public static ListNode mergeSort(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        ListNode rightHead=slow.next;
+        ListNode leftHead=head;
+        slow.next=null;
+        ListNode newLeft=mergeSort(leftHead);
+        ListNode newRight=mergeSort(rightHead);
+        return merge(newLeft, newRight);
+
+    }
+
+    public static ListNode merge(ListNode leftHead, ListNode rightHead){
+        ListNode merged=new ListNode(-1);
+        ListNode temp=merged;
+
+        while(leftHead!=null && rightHead!=null){
+            if(leftHead.val<rightHead.val){
+                temp.next=leftHead;
+                leftHead=leftHead.next;
+            }else{
+                temp.next=rightHead;
+                rightHead=rightHead.next;
+            }
+            temp=temp.next;
+        }
+
+        while(leftHead!=null){
+                temp.next=leftHead;
+                leftHead=leftHead.next;
+                temp=temp.next;
+        }
+        while(rightHead!=null){
+                temp.next=rightHead;
+                rightHead=rightHead.next;
+                temp=temp.next;
+        }
+
+        return merged.next;
+    }
+    public ListNode sortList(ListNode head) {
+        ListNode slow=head;
+        if(head==null){
+            return head;
+        }
+
+        return mergeSort(head);
+
+    }
+}
