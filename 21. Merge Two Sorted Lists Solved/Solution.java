26f3c96f74f39f6b25c9669bbd80b9825ae28f53
Sumeet Singh <148633096+thesumeetsingh@users.noreply.github.com>
2026-09-03 03:44:59 +0530

    Add solution 21. Merge Two Sorted Lists Solved

---
+++ 21. Merge Two Sorted Lists Solved/Solution.java
@@ -0,0 +1,178 @@
+LeetCode Logo
+Problem List
+Debugging...
+Debugging...
+
+
+
+
+
+
+
+
+
+0
+
+avatar
+Avatar
+Sumeet Singh
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+Premium
+Description
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+Code
+Accepted
+Accepted
+Testcase
+Testcase
+Test Result
+21. Merge Two Sorted Lists
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+6,625,500/9.6M
+Acceptance Rate
+68.8%
+Topics
+icon
+Companies
+Similar Questions
+Discussion (633)
+
+
+Choose a type
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Read more
+
+Copyright © 2026 LeetCode. All rights reserved.
+
+25.4K
+
+
+633
+
+230 Online
+Java
+Auto
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+    }
+}
+222120191817161514131211109876543
+Saved
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Case 3
+Input
+list1 =
+[1,2,4]
+list2 =
+[1,3,4]
+Output
+[1,1,2,3,4,4]
+Expected
+[1,1,2,3,4,4]
+Contribute a testcase
+Search questions
+
+
+Submit
+Ctrl
+Enter
+Submit
+Ctrl
+Enter
+Accepted — reading current code…
