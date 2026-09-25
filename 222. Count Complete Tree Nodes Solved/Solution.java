c8af9a3e5af13fb394594fe01333fdc886029d88
Sumeet Singh <148633096+thesumeetsingh@users.noreply.github.com>
2026-09-25 15:39:02 +0530

    Add solution 222. Count Complete Tree Nodes Solved

---
+++ 222. Count Complete Tree Nodes Solved/Solution.java
@@ -0,0 +1,170 @@
+LeetCode Logo
+Problem List
+Debugging...
+Debugging...
+
+
+
+
+
+
+
+
+
+
+0
+
+avatar
+Avatar
+Sumeet Singh
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+Premium
+Description
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+Code
+Testcase
+Testcase
+Test Result
+222. Count Complete Tree Nodes
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+1,254,918/1.7M
+Acceptance Rate
+72.4%
+Topics
+icon
+Companies
+Similar Questions
+Discussion (237)
+
+
+Choose a type
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Copyright © 2026 LeetCode. All rights reserved.
+
+9.7K
+
+
+237
+
+21 Online
+Java
+Auto
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        
+    }
+}
+1234567891011121314151617181920
+Saved
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Case 3
+Input
+root =
+[1,2,3,4,5,6]
+Output
+6
+Expected
+6
+Contribute a testcase
+Search questions
+
+
+Accepted — reading current code…
